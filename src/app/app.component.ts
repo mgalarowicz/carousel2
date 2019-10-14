ee073f642ea35a0e87ba1221a1d27f0ff4a30801
Marek Galarowicz <marek.galarowicz@soneta.pl>
2019-10-14 09:58:31 +0200

    TwoSliders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,25 +9,77 @@ import { WheelSelectorComponent } from '@hyperblob/ngx-wheel-selector';
 export class AppComponent {
   @ViewChild('wheelSelector', {static: false}) wheelSelector: WheelSelectorComponent;
 
- data: Array<any> = [
-    {
-      currentIndex: 0,
-      weight: 10,
-      list: [
-        'Plan A', 'Plan B', 'Plan C', 'Plan D'
-      ],
-      onClick: this.clickOnPlan.bind(this),
-      textAlign: 'center',
-      className: 'row-group'
-    }
-  ];
+//  data: Array<any> = [
+//     {
+//       currentIndex: 0,
+//       weight: 10,
+//       list: [
+//         'Plan A', 'Plan B', 'Plan C', 'Plan D'
+//       ],
+//       onClick: this.clickOnPlan.bind(this),
+//       textAlign: 'center',
+//       className: 'row-group'
+//     }
+//   ];
+
+data: Array<any> = [
+  {
+    currentIndex: 0,
+    weight: 3,
+    list: [
+      'Plan A - free', 'Plan B - $50', 'Plan C - $100'
+    ],
+    onClick: this.clickOnPlan.bind(this),
+    textAlign: 'center',
+    className: 'row-group'
+  },
+  {
+    divider: true,
+    weight: 1,
+    text: 'product',
+    textAlign: 'center',
+    className: 'divider'
+  },
+  {
+    currentIndex: 2,
+    weight: 3,
+    list: [
+      '1 * A item', '2 * A items', '3 * A items', '4 * A items', '5 * A items'
+    ],
+    onClick: this.clickOnProduct.bind(this),
+    textAlign: 'center',
+    className: 'item-group'
+  }
+]
 
   constructor() {
     /*noop*/
   }
 
-  dataChange({ gIndex, iIndex }) {
-    console.log('list', gIndex, iIndex);
+  // dataChange({ gIndex, iIndex }) {
+  //   console.log('list', gIndex, iIndex);
+  // }
+
+  dataChange ({ gIndex, iIndex }) {
+    console.log('list', gIndex, iIndex)
+    if (gIndex === 0) {
+      let currentIndex = 0
+      let list = []
+      switch (iIndex) {
+        case 2:
+          list = ['C item 1', 'C item 2', 'C item 3', 'C item 4', 'C item 5', 'C item 6', 'C item 7', 'C item 8', 'C item 9']
+          currentIndex = 4
+          break
+        case 1:
+          list = ['1 * B item', '2 * B items', '3 * B items', '4 * B items', '5 * B items', '6 * B items', '7 * B items']
+          currentIndex = 3
+          break
+        default:
+          list = ['1 * A item', '2 * A items', '3 * A items', '4 * A items', '5 * A items']
+          currentIndex = 2
+      }
+      this.wheelSelector.setGroupData(2, { ...this.data[2], ...{ currentIndex, list }})
+    }
   }
 
   clickOnPlan(gIndex, iIndex) {
@@ -35,13 +87,19 @@ export class AppComponent {
     alert('Clicked plan: ' + this.data[gIndex].list[iIndex]);
   }
 
+  clickOnProduct (gIndex, iIndex) {
+    alert('Clicked product: ' + this.data[gIndex].list[iIndex])
+  }
+
   confirm() {
     const ciList = this.wheelSelector.getCurrentIndexList();
     const planDetail = this.data[0].list[ciList[0]];
+    const productDetail = this.data[2].list[ciList[2]];
 
     alert(
       'Confirmed index list: ' + ciList + '.\n' +
-      'Confirmed plan: ' + planDetail + '.\n'
+      'Confirmed plan: ' + planDetail + '.\n' +
+      'Confirmed product: ' + productDetail
     );
 }
 }
